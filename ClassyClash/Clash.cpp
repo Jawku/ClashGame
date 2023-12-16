9e6b7e8632ea437097dd97151d9ab921d96a0808
Jawku <151454816+Jawku@users.noreply.github.com>
2023-12-16 21:05:25 +0100

    59 - Map movement with usage of vectors
    Added raymath.h library, defined movement with wsad and added that movement as vector update to move map around

--- ClassyClash/Clash.cpp
+++ ClassyClash/Clash.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include "raylib.h"
+#include "raymath.h"
 
 
 
@@ -12,6 +13,7 @@ InitWindow(WindowDimension[0],WindowDimension[1],"Clash Of Titi");
 // Loading map:
 Texture2D Map = LoadTexture("Textures/FirstMap.png");
 Vector2 MapPos{0.0,0.0};
+float Speed = 4.0;
 
 
 
@@ -21,6 +23,28 @@ while (!WindowShouldClose())
 {   
     BeginDrawing();
     ClearBackground(WHITE);
+    Vector2 Direction{};
+
+    if (IsKeyDown(KEY_A)) Direction.x -= 1.0;
+    if (IsKeyDown(KEY_D)) Direction.x += 1.0;
+    if (IsKeyDown(KEY_S)) Direction.y += 1.0;
+    if (IsKeyDown(KEY_W)) Direction.y -= 1.0;
+
+    if (Vector2Length(Direction) != 0.0 )
+    {
+        // Set MapPos based on our movement vector so that we can move our map around
+        MapPos = Vector2Subtract(MapPos,Vector2Scale(Vector2Normalize(Direction),Speed));
+    }
+    
+    
+        
+        
+    
+    
+
+
+
+
     
     DrawTextureEx(Map,MapPos,0,4,WHITE);
 
