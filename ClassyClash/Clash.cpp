2c02f65ce1989319f4aca929ad5961da6e619d09
Jawku <151454816+Jawku@users.noreply.github.com>
2023-12-17 17:38:10 +0100

    Animation for movement added

--- ClassyClash/Clash.cpp
+++ ClassyClash/Clash.cpp
@@ -16,12 +16,21 @@ Texture2D Map = LoadTexture("Textures/FirstMap.png");
 Vector2 MapPos{0.0,0.0};
 float Speed = 4.0;
 
-// Load character:
+                                            // Load character:
 Texture2D KnightRun = LoadTexture("Textures/characters/knight_run_spritesheet.png");
-Texture2D Knight = LoadTexture("Textures/characters/knight_idle_spritesheet.png");
+Texture2D KnightIdle = LoadTexture("Textures/characters/knight_idle_spritesheet.png"); 
+Texture2D Knight{}; 
 Vector2 KnightPos{
     (float)WindowDimension[0]/2.0f - 4.0f* (0.5f * (float)Knight.width/6.0f),
     (float)WindowDimension[1]/2.0f - 4.0f * (0.5f * (float)Knight.height)};
+// 1: facing right, -1 facing left
+float rightLeft{1};
+const int maxFrame{6};
+int frame{0};
+const float updateTime{1.f/12.f};
+float runningTime{};
+
+
 
 
 
@@ -33,17 +42,9 @@ while (!WindowShouldClose())
 {   
     BeginDrawing();
     ClearBackground(WHITE);
+    const float dT = GetFrameTime();   
     Vector2 Direction{};
-    Rectangle Source{0.f, 0.f, (float)Knight.width/6.f, (float)Knight.height};
-
-    Rectangle Dest{
-                    KnightPos.x,
-                    KnightPos.y,
-                    4.0f * (float)Knight.width/6.f,
-                    4.0f * (float)Knight.height
-                    };
-
-    Vector2 origin{};
+    
 
     if (IsKeyDown(KEY_A)) Direction.x -= 1.0;
     if (IsKeyDown(KEY_D)) Direction.x += 1.0;
@@ -53,14 +54,36 @@ while (!WindowShouldClose())
     if (Vector2Length(Direction) != 0.0 )
     {
         // Set MapPos based on our movement vector so that we can move our map around
-        MapPos = Vector2Subtract(MapPos,Vector2Scale(Vector2Normalize(Direction),Speed));       
+        MapPos = Vector2Subtract(MapPos,Vector2Scale(Vector2Normalize(Direction),Speed));
+
+        //Ternary operator setting up our facing direction based on movement direction
+        Direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+        Knight = KnightRun;               
+    }
+    else
+    {
+        Knight = KnightIdle;
     }
     
-    
-    // Draw the map
+       // Draw the map
     DrawTextureEx(Map,MapPos,0,4,WHITE);
 
+
+    // Update animation frame variables:
+    runningTime += dT;
+    if (runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.f;
+
+        if (frame > maxFrame) frame = 0;
+    }
+    
+
     //Draw Knight
+    Rectangle Source{(float)Knight.width/6.f * frame, 0.f, rightLeft * (float)Knight.width/6.f, (float)Knight.height};
+    Rectangle Dest{KnightPos.x, KnightPos.y, 4.0f * (float)Knight.width/6.f, 4.0f * (float)Knight.height};
+    Vector2 origin{};
     DrawTexturePro(Knight,Source,Dest,origin,0.f,WHITE);
 
 
