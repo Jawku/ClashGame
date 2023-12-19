f595d29768273881f196f947a61d69124c0bd08f
Jawku <151454816+Jawku@users.noreply.github.com>
2023-12-19 18:34:54 +0100

    Finish of character class
    Finalized character class and everything used in the main function. Cleared unnecessary data from that as well.

--- ClassyClash/Clash.cpp
+++ ClassyClash/Clash.cpp
@@ -13,8 +13,8 @@ private:
     Texture2D texture{LoadTexture("Textures/characters/knight_idle_spritesheet.png")};
     Texture2D idle{LoadTexture("Textures/characters/knight_idle_spritesheet.png")};
     Texture2D run{LoadTexture("Textures/characters/knight_run_spritesheet.png")};
-    Vector2 screenPos;
-    Vector2 worldPos;
+    Vector2 screenPos{};
+    Vector2 worldPos{};
 
     // 1: facing right, -1 facing left
     float rightLeft{1};
@@ -24,7 +24,7 @@ private:
     const float updateTime{1.f / 12.f};
     float runningTime{};
 
-    const float speed{4.f};    
+    const float speed{4.f};
 };
 
 void character::setScreenPos(int winWidth, int winHeight)
@@ -71,6 +71,12 @@ void character::tick(float deltaTime)
         if (frame > maxFrame)
             frame = 0;
     }
+
+    // Draw Knight
+    Rectangle Source{(float)texture.width / 6.f * frame, 0.f, rightLeft * (float)texture.width / 6.f, (float)texture.height};
+    Rectangle Dest{screenPos.x, screenPos.y, 4.0f * (float)texture.width / 6.f, 4.0f * (float)texture.height};
+    Vector2 origin{};
+    DrawTexturePro(texture, Source, Dest, origin, 0.f, WHITE);
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main()
@@ -82,72 +88,27 @@ int main()
     // Loading map:
     Texture2D Map = LoadTexture("Textures/FirstMap.png");
     Vector2 MapPos{0.0, 0.0};
-    float Speed = 4.0;
 
-    // Load character:
-    Texture2D KnightRun = LoadTexture("Textures/characters/knight_run_spritesheet.png");
-    Texture2D KnightIdle = LoadTexture("Textures/characters/knight_idle_spritesheet.png");
-    Texture2D Knight{};
-    Vector2 KnightPos{
-        (float)WindowDimension[0] / 2.0f - 4.0f * (0.5f * (float)Knight.width / 6.0f),
-        (float)WindowDimension[1] / 2.0f - 4.0f * (0.5f * (float)Knight.height)};
-    // 1: facing right, -1 facing left
-    float rightLeft{1};
-    const int maxFrame{6};
-    int frame{0};
-    const float updateTime{1.f / 12.f};
-    float runningTime{};
-
+    // Initialization of our character class variable:
+    character knight;
+    // setting up screenPos -> where in window we will draw our character
+    knight.setScreenPos(WindowDimension[0],WindowDimension[1]);
+    
     SetTargetFPS(120);
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
-        const float dT = GetFrameTime();
-        Vector2 Direction{};
 
-        if (IsKeyDown(KEY_A))
-            Direction.x -= 1.0;
-        if (IsKeyDown(KEY_D))
-            Direction.x += 1.0;
-        if (IsKeyDown(KEY_S))
-            Direction.y += 1.0;
-        if (IsKeyDown(KEY_W))
-            Direction.y -= 1.0;
-
-        if (Vector2Length(Direction) != 0.0)
-        {
-            // Set MapPos based on our movement vector so that we can move our map around
-            MapPos = Vector2Subtract(MapPos, Vector2Scale(Vector2Normalize(Direction), Speed));
-
-            // Ternary operator setting up our facing direction based on movement direction
-            Direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
-            Knight = KnightRun;
-        }
-        else
-        {
-            Knight = KnightIdle;
-        }
+        // We need to move our map in opposit direction of our WorldPos
+        // World pos - vector that defines where we want to move our character.        
+        MapPos = Vector2Scale(knight.getWorldPos(),-1.f);
 
         // Draw the map
         DrawTextureEx(Map, MapPos, 0, 4, WHITE);
 
-        // Update animation frame variables:
-        runningTime += dT;
-        if (runningTime >= updateTime)
-        {
-            frame++;
-            runningTime = 0.f;
-
-            if (frame > maxFrame)
-                frame = 0;
-        }
-
-        // Draw Knight
-        Rectangle Source{(float)Knight.width / 6.f * frame, 0.f, rightLeft * (float)Knight.width / 6.f, (float)Knight.height};
-        Rectangle Dest{KnightPos.x, KnightPos.y, 4.0f * (float)Knight.width / 6.f, 4.0f * (float)Knight.height};
-        Vector2 origin{};
-        DrawTexturePro(Knight, Source, Dest, origin, 0.f, WHITE);
+        // Doing evrything that has to be done each frame in our character class with usage of tick function
+        knight.tick(GetFrameTime());
 
         EndDrawing();
     }
